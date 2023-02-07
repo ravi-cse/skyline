e04a8d3f956a8e224389075c020dce7559a0d315
Ravi Chaudhary <ravichaudhary.cse@gmail.com>
2023-02-07 23:19:42 +0530

    Add files via upload

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,285 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <!-- META TAGS -->
+
+    <meta name="author" content="Ravi Chaudhary">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=5">
+
+    <!-- WEBSITE TITLE & DESCRIPTION -->
+    <title>Skyline Contractor & Builders - Real Estate Landing Page</title>
+    <meta name="description" content="Showcase your luxurious apartments with this luxurious template">
+    <meta name="keywords" content="real estate, landing page, ecommerce, business, home, house">
+
+    <!-- OG meta tags that improve the look of your post on social media -->
+    <meta property="og:site_name" content="">
+    <!--website name-->
+    <meta property="og:site" content="">
+    <!--website link-->
+    <meta property="og:title" content="">
+    <!--Post title-->
+    <meta property="og:description" content="">
+    <!--Post description-->
+    <meta property="og:image" content=""><!-- Image link (jpg only)-->
+    <meta property="og:url" content="">
+    <!--where do you want your post to link to-->
+    <meta property="og:type" content="article">
+
+    <link rel="icon" type="image/png" sizes="96x96" href="./public/images/favicon.png">
+    <!-- STYLES -->
+    <!-- Bootstrap -->
+    <link href="./public/bootstrap.min.css" rel="stylesheet">
+    <!-- Linearicoms -->
+    <link href="./public/linearicons.min.css" rel="stylesheet">
+    <!-- Animations -->
+    <link href="./public/animations.css" rel="stylesheet">
+    <!-- Lightbox -->
+    <link href="./public/lightbox.min.css" rel="stylesheet">
+    <!-- Video Lightbox -->
+    <link href="./public/modal-video.min.css" rel="stylesheet">
+    <!-- Main Stylesheet -->
+    <link href="./public/style.css" rel="stylesheet">
+</head>
+
+<body cz-shortcut-listen="true">
+    <!-- Loading Screen -->
+    <div id="loader-wrapper" class="loaded">
+        <img class="loader-logo img-fluid" src="./public/city.svg" alt="lading screen logo">
+        <p class="loader-logo">Skyline Contractor & Builders</p>
+        <div id="progress" style="width: 100%;"></div>
+        <p class="loader-text">LOADING</p>
+    </div>
+
+    <!-- //// HEADER //// -->
+    <header id="main-header">
+        <nav class="navbar fixed-top navbar-expand-lg navbar-light scrolled">
+            <div class="container">
+                <!-- Site Logo -->
+                <a id="main-logo" class="navbar-brand" href="#">Skyline</a>
+                <!-- Dropdown Button -->
+                <button id="hamburger" class="navbar-toggler" type="button" data-toggle="collapse"
+                    data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+                    aria-label="Toggle navigation">
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <!-- Navigation Links -->
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav ml-auto">
+                        
+                        <li class="nav-item">
+                            <a class="nav-link" href="#about-us">About Us</a>
+                        </li>
+                        <li class="nav-item">
+                        <li class="nav-item">
+                            <a class="nav-link" href="#features">Our Services</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#cta">Contact Us</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    </header><!-- Header End -->
+
+    <!-- /// HERO SECTION /// -->
+    <div id="hero-section">
+        <div class="row hero-unit angled-section large-margin">
+            <div class="container">
+                <div class="hero-caption">
+                    <!-- Main Tagline -->
+                    <div class="animation-element pop-in in-view">
+                        <img id="logo" src="./public/city.svg" class="img-fluid" alt="logo">
+                    </div>
+                    <div class="animation-element slide-down in-view">
+                        <h1>Skyline Contractor & Builders</h1>
+                        <p>Engineering | Construction | Contracts</p>
+                    </div>
+                    <a class="button" href="#cta">Contact Us</a>
+                    <i id="arrow" class="lnr lnr-chevron-down colored"></i>
+                </div>
+            </div>
+        </div>
+    </div><!-- Hero Section End -->
+
+
+    <div class="container">
+        <!-- /// Container /// -->
+        <!-- /// ABOUT SECTION /// -->
+        <div class="large-margin">
+            <a id="about-us"></a><!-- Nav Anchor -->
+            <div class="row justify-content-center tiny-margin">
+                <h2 class="heading">About Us</h2>
+                <p>
+                We started our construction business some twenty years ago and now we have a handful experience of Construction, Repairing, Renovation,Interiors,Painting and other things related to different types of buildings. We are into construction, renovation, restoration and many other tasks for the exterior and interior of the building which we have developed over the period of these two decades.
+                </p>
+                <p>
+                    There are many jobs in the construction and renovation industry and the customers always want to have all the things related to Renovation,Painting and Interiors under one roof. We can construct the buildings whether it is residential, commercial, or industrial. Along with construction, we also provide the services of renovation, restoration, and other thing related to the existing buildings.
+                </p>
+                <p>
+                    We take up the contract of plastering and painting the house. If the house owner wants to change his flooring, our staff is an expert in that also and will provide the floor as per the requirement of the customer. We use the perfect materials that will last longer and the customers will have no complaints after the construction or renovation has been completed.
+                </p>
+                <p>
+                There are many things involved in construction and renovation which include planning and execution of the project, transferring of materials, plumbing, wiring, and many other things. Our clients are happy with our services as we provide good materials and services, which are durable for a long time.
+                </p>
+                <p>
+                    Our company has a good turnover because of the faith that the customers have on us. We provide all the services at a reasonable rate so the customers should not worry about money. We complete our job as fast as possible and with accuracy so that the work can be completed on or before time.
+                </p>
+            </div>
+            <div class="row text-center medium-margin">
+
+        </div>
+    </div> <!-- /// Container End /// -->
+
+    <div class="container">
+        <!-- /// Container /// -->
+        <!-- /// ABOUT SECTION /// -->
+        <div id="about-section" class="large-margin">
+            <a id="features"></a><!-- Nav Anchor -->
+            <div class="row justify-content-center tiny-margin">
+                <h2 class="heading">Our Services</h2>
+            </div>
+            <div class="row text-center medium-margin">
+                <!-- Feature Card -->
+                <div class="col-md-4">
+                    <div class="feature reset animation-element slide-down in-view">
+                        <i class="lnr lnr-apartment"></i>
+                        <h3 class="short-hr-center">Civil Work</h3>
+                        <p>We are a civil work Contractor and Builder that take various contracts related to construction or renovation of  a building. If the building is to be constructed, then we design it as per the requirement of our client and bring that design into reality. Our staff understands all the processes of civil work related to construction so that the output should satisfy the client.</p>
+                    </div>
+                </div>
+                <!-- Feature Card -->
+                <div class="col-md-4">
+                    <div class="feature animation-element slide-down delayed in-view">
+                        <i class="lnr lnr-diamond"></i>
+                        <h3 class="short-hr-center">Renovation & Painting Service</h3>
+                        <p>
+                            We are commercial renovation & painting contractor and provide the services of painting of interior and exterior of a house, commercial building, industrial building, etc. Our painters are specialized in their task and provide good result. The competitive pricing that we provide will help the customers to paint their houses in a cost-effective manner. The types of painters available with us are mentioned here.
+                        </p>
+                    </div>
+                </div>
+                <!-- Feature Card -->
+                <div class="col-md-4">
+                    <div class="feature animation-element slide-down delayed-1 in-view">
+                        <i class="lnr lnr-lock"></i>
+                        <h3 class="short-hr-center">Wood Work</h3>
+                        <p>We provide woodwork during the construction or renovation of the house. Woodwork includes cupboards, doors, windows, ventilators, and many other things. After construction, they need varnishing and painting as they cannot be damaged by termites. We use best wooden material to construct the doors and windows of the house. We also take care of the fact where the wooden work should be done and where to be avoided.</p>
+                    </div>
+                </div>
+            </div>
+            </div>
+        </div>
+    </div> <!-- /// Container End /// -->
+    <!-- <div id="testimonial-section" class="large-margin">
+        <div class="container">
+            <div class="row">
+                <div class="testimonial-wrapper text-center animation-element fade-in in-view">
+                    <h2><span class="colored">"</span> MY DREAM APARTMENT <span class="colored">"</span></h2>
+                    <p>Lorem ipsum dolor sit amet, cu assentior voluptaria consequuntur usu, ut nam assum verterem, his
+                        unum scripserit suscipiantur no. Graeci fabellas vix at. Et mei quidam fuisset, vis purto
+                        percipit cu. Pro audire consequuntur an, eum id consul scribentur. Dico delectus adipiscing id
+                        eum. Paulo alterum assueverit an pri, errem zril expetenda at pro.</p>
+                    <p class="name">JOHN DOE</p>
+                </div>
+            </div>
+        </div>
+    </div> -->
+    
+    <!-- /// CTA SECTION /// -->
+    <div id="cta-section">
+        <div class="container">
+            <a id="cta"></a><!-- Nav Anchor -->
+            <div class="row justify-content-center tiny-margin text-center">
+                <h1 class="short-hr-center">Contact Us</h1>
+            </div>
+            <div class="row">
+                <div class="col-md-6 text-center">
+                    <form id="contactForm" data-toggle="validator" novalidate="true">
+                        <div class="form-group">
+                            <!-- First Name Field -->
+                            <input type="text" id="name" placeholder="Name*" required="" size="35"
+                                data-error="Name is required">
+                            <div class="help-block with-errors"></div>
+                        </div>
+                        <div class="form-group">
+                            <!-- Phone Number Field -->
+                            <input type="tel" id="phone" placeholder="Phone Number*" required="" size="35"
+                                data-error="Phone Number is required">
+                            <div class="help-block with-errors"></div>
+                        </div>
+                        <div class="form-group">
+                            <!-- Email Field -->
+                            <input type="email" id="email" required="" placeholder="Email*"
+                                data-error="Email is required" size="35">
+                            <div class="help-block with-errors"></div>
+                        </div>
+                        <div class="form-group">
+                            <!-- Message Field -->
+                            <textarea id="message" name="message" placeholder="Message"
+                                data-error="Message cannot be empty"></textarea>
+                            <div class="help-block with-errors"></div>
+                            <p class="subtle">*required fields</p>
+                            <!-- Submit Button -->
+                            <button type="submit" class="button disabled">FIND YOUR NEW HOUSE</button>
+                            <!-- Success Message -->
+                            <div id="msgSubmit" class="text-center hidden"></div>
+                        </div>
+                    </form>
+                </div>
+                <div class="col-md-6 text-center">
+                    <div class="agent-card">
+                        <div class="img-fluid" alt="agent-photo">
+                            <i class="lnr lnr-apartment"></i>
+                        </div>
+                        <!-- <img src="./public/agent.jpg" class="img-fluid" alt="agent-photo"> -->
+                        <p class="agent-name short-hr-center">Skyline Contractor & Builders</p>
+                        <p><b>Phone:</b> +91 95601 91810</p>
+                        <p><b>Email:</b> veer948001@gmail.com</p>
+                        <p><b>Address:</b> Shop No 1, NH8, Village Rampura, <br/>P.O. Shikhopur, Gurugram 12004</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div id="footer">
+            <div class="row">
+                <div class="col-md-12 text-center">
+                    <p>Copyright Skyline Contractor & Builders <span id="year">2020</span></p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- //// SCRIPTS //// -->
+    <script src="./public/jquery-3.2.1.min.js"></script>
+    <script src="./public/popper.min.js"></script>
+    <script src="./public/bootstrap.min.js"></script>
+    <script src="./public/blazy.min.js"></script>
+    <script src="./public/lightbox.min.js"></script>
+    <script src="./public/jquery-modal-video.min.js"></script>
+    <script src="./public/validator.min.js"></script>
+    <script src="./public/main.js"></script>
+    <!--     
+    <div id="lightboxOverlay" class="lightboxOverlay" style="display: none;"></div>
+    <div id="lightbox" class="lightbox" style="display: none;">
+        <div class="lb-outerContainer">
+            <div class="lb-container"><img class="lb-image"
+                    src="data:image/gif;base64,R0lGODlhAQABAIAAAP///wAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw==">
+                <div class="lb-nav"><a class="lb-prev" href="#"></a><a class="lb-next" href="#"></a>
+                </div>
+                <div class="lb-loader"><a class="lb-cancel"></a></div>
+            </div>
+        </div>
+        <div class="lb-dataContainer">
+            <div class="lb-data">
+                <div class="lb-details"><span class="lb-caption"></span><span class="lb-number"></span></div>
+                <div class="lb-closeContainer"><a class="lb-close"></a></div>
+            </div>
+        </div>
+    </div> -->
+</body>
+
+</html>
